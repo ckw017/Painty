95cdecfcd3c4ea952b912390deefa235811dd4b6
ckw017 <christopherwong888@gmail.com>
2017-03-24 21:51:24 -0700

    Fixed missing MouseInputListener

--- src/MouseInputListener.java
+++ src/MouseInputListener.java
@@ -1,7 +1,7 @@
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class MoeseInputListener extends MouseAdapter{
+public class MouseInputListener extends MouseAdapter{
 	private boolean pressed;
 
 	public boolean isPressed(){
